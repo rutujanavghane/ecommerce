7f98817531abd04e1aa9187c43d88d3c8ae3493e
rutujanavghane <88076622+rutujanavghane@users.noreply.github.com>
2022-04-26 23:36:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom';
+//import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
+import Modal from './components/Modal';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProductList}></Route>
+          <Route path="/details" component={Details}></Route>
+          <Route path="/cart" component={Cart}></Route>
+          <Route component={Default}></Route>
+        </Switch>
+        <Modal />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
+window.addEventListener('load', e => {
+  
+  registerSW(); 
+});
+
+async function registerSW() { 
+  if ('serviceWorker' in navigator) { 
+    try {
+      await navigator.serviceWorker.register('./sw.js'); 
+    } catch (e) {
+      alert('ServiceWorker registration failed. Sorry about that.'); 
+    }
+  } else {
+    document.querySelector('.alert').removeAttribute('hidden'); 
+  }
+}
